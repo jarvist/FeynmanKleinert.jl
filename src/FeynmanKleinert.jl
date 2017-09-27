784b2e3dd7b474e15cfde8d665b99f035e4310fa
Jarvist Moore Frost <jarvist@gmail.com>
2017-09-27 22:19:30 +0100

    Only partially moved over from Workbook. NOT WORKING.
    But it is late and I am tired.

--- src/FeynmanKleinert.jl
+++ src/FeynmanKleinert.jl
@@ -1,5 +1,89 @@
 module FeynmanKleinert
 
-# package code goes here
+# Bring out the major leagues... https://www.youtube.com/watch?v=_E6DDktoPhg
+using Optim
+# Pkg.status("Optim.jl") --> Optim                         0.7.8              pinned.250b50ab.tmp
+# I've pinned this to a weird old version... must update signature at some point to the new bindings
+
+using QuadGK # For numerical integration
+
+export K,Va2,Wtilde,verboseWtilde,W # export all the things.
+
+"Integrand of (4) in Feynman and Kleinert"
+K(xp,x,a2,V)=1/sqrt(2*π*a2)*exp(-(x-xp)^2/(2*a2))*V(xp)
+"(4) in Feynman and Kleinert"
+Va2(x,a2,V)=quadgk(xp->K(xp,x,a2,V), -Inf, +Inf) #, reltol=0, abstol=1e-5)
+
+# (5) naively as written, in Feynman and Kleinert
+#Wtilde(x0,a2,Ω,g,β)=(1/β) * log( sinh(β*Ω/2)/(β*Ω/2) ) - (Ω^2/2) * a2 + Va2(x0,a2,g)[1]
+# Large β limit; by taking sinh(x)=e^x-e^-x , and then dropping e^-x
+#Wtilde(x0,a2,Ω,g,β)=(1/β) * (β*Ω/2 - log(β*Ω/2)) - (Ω^2/2) * a2 + Va2(x0,a2,g)[1]
+
+# Branch depending on β; for accuracy + lack of infinities
+"""
+    Wtilde(x0,a2,Ω,V,β)
+
+(5) in Feynman and Kleinert. Switches depending on D=β*Ω/2 to use either an exponential approximation for sinh(x) (D>7.5) or an explicit evaluation and then take logarithms. Without doing this you get collapse to infinity for large β.
+"""
+function Wtilde(x0,a2,Ω,V,β)
+    D=β*Ω/2
+    if D>7.5
+        F=(1/β) * (D - log(2*D)) # Large limit;  by taking sinh(x)=e^x-e^-x , and then dropping e^-x
+    else
+        F=(1/β)*( log(sinh(D)/D) ) # explicit evaluation, naively as written in (5)
+    end
+    V0=Va2(x0,a2,V())[1]
+    mid=(Ω^2/2) * a2
+    return F-mid+V0
+end
+
+"""
+   verboseWtilde(x0,a2,Ω,g,β)
+
+Expanded version of Wtilde, reporting the decomposition - this was used to explain what was going wrong
+"""
+function verboseWtilde(x0,a2,Ω,g,β)
+    D=β*Ω/2
+    println("D=β*Ω/2 = $D")
+    F= (1/β)*( log(sinh(D)) - log(D) )
+    #F= (1/β)*( (log(2D)+D^2/6+D^4/180) - log(D) ) # generalised Puiseux series
+    # Large D limit, through sinh(D) --> e^D [through away e^-D part]
+    F = (1/β) * (D - log(2*D))
+    V=Va2(x0,a2,g)[1]
+    mid=(Ω^2/2) * a2
+    println("Wtilde(x0=$x0,a2=$a2,Ω=$Ω,g=$g,β=$β) \n\t= F - mid + V")
+    println("\t= $F - $mid + $V = ",F-mid+V)
+    return F-mid+V
+end
+
+"""
+    W(x0,g,β)
+
+    Implementing optimisation / minimisation described in Feynman-Kleinert (6)
+    Calculates optimal W for x0, with given g and Beta
+    Let's try and use (7) (a2 in terms of omega) to fix prior problems...
+"""
+function W(x0,g,β)
+    #println("W(x0=$x0,g=$g,β=$β)")
+    
+    a2(Ω)=1/(β*Ω^2)*( (β*Ω)/2 * coth(β*Ω/2)-1 ) #(7)
+    myf(x)=Wtilde(x0,a2(x[1]),x[1],g,β)
+    
+    lower=[0.0]
+    upper=[5.0]
+    initial=[1.0]
+
+    res=optimize(OnceDifferentiable(myf), 
+        initial, lower, upper, Fminbox(); 
+        optimizer=GradientDescent, 
+        optimizer_o=(Optim.Options(autodiff=true,allow_f_increases=true)) )
+    minimum=Optim.minimum(res)
+    Ω=Optim.minimizer(res)[1]
+    mya2=a2(Ω)[1]
+    #println("Optimised for W(x0=$x0,g=$g,β=$β). \n\tΩ=$Ω, a2=$mya2, minimum=$minimum.")
+    #show(res)
+    return minimum
+end
+
 
 end # module
---
+++ test/Feynman-Kleinert-1986-PRA.jl
@@ -0,0 +1,127 @@
+# A work in progress - beware, dragons!
+# Feynman-Kleinert-1986-PRA
+#
+# Workbook to reimplement Feynman and Klenert's 1986 PRA "Effective classical partition function"
+
+using FeynmanKleinert
+
+# https://doi.org/10.1103/PhysRevA.34.5080
+#
+# Effective classical partition functions. R. P. Feynman and H. Kleinert. Phys. Rev. A 34, 5080 â Published 1 December 1986
+#
+# Errata
+#
+# The form for the double well potential (page 34, RHS, third paragraph starting 'Another example is the double-well...'), should read
+#
+# $V(x)=-\frac{1}{2} x^2 + \frac{1}{4} g x^4 + \frac{1}{4g}$.
+#
+# Strangely this is correct in the captions of figure 2 and 3!
+
+# Nb: Forked from Workbook by Jarv 2017-09-27
+
+# Variables
+# Potential energy g; either anharmonic strength or double-well setup
+g=0.1976
+# Thermodynamic Beta, natch
+β=10
+
+# Potential energy curves - V(x) functions
+# Double well potential as given in figure 2 + 3 captions
+V(x,g)=-0.5*x^2 + 0.25*g*x^4 + 0.25/g
+V(x)=V(x,g)
+
+# Anharmonic potential, following Kleinhert's book (p. 471); Also Figure 1 Feynman-Kleinert
+#V(x,g)=x^2/2+x^4*g/4
+
+# Harmonic oscillator - simple soln
+#V(x,g)=0.5*x^2
+
+println("V(0.0) = ",V(0.0,g))
+println("Va2(0.0,1.0) = ",Va2(0.0,1.0,V))
+
+using Plots
+
+# OK, let's have a look our actors:
+# V, the bare potential
+# Va2, the Gaussian-smoothed potential
+# Wtilde, the Auxillary potential, including partition function magic / failure
+#
+xrange=-3:0.1:3
+#
+plot(x->V(x,g),xrange,label="V") # bare [potential]
+#
+for a2 in 0.5 #:0.1:0.5 # Gaussian smearing widths to apply
+    plot!(x->Va2(x,a2,V)[1],xrange,label="Va2, a2=$a2")
+    plot!(x->Wtilde(x,a2,5,V,β),xrange,label="Wtilde, a2=$a2") # bare [potential]
+end
+
+#plot!(ylim=(0,1)) # force display
+plot!()
+
+#### Print out a table of values for omega and A2 values ####
+g=0.4
+β=10
+
+@printf("Wtilde(x0,a2,Ω,g=%f,β=%f)\n",g,β)
+
+@printf("a2\\Ω")
+for Ω in 0.1:0.2:2.0
+    @printf(" %.3f",Ω)
+end
+
+for a2 in 0.1:0.1:2.0
+    @printf("\n a2=%.3f",a2)
+    for Ω in 0.1:0.2:2.0
+        @printf(" %.3f",Wtilde(0.0,a2,Ω,V,β))
+    end
+end
+
+# Replot fits from Feynman-Kleinert Table II
+# - basically checking whether the Wtilde equation is reporting correctly, 
+# and whether (as described in the caption) only Wtilde for g=0.1976 has double well structure 
+@printf("Feynman-Kleinert Table II reads:\n g      E0=Wtilde\n 0.1976 0.650 \n 0.4    0.549 \n 4.0    0.598 \n 40.0   1.409\n")
+β=3000
+# Much higher than this, and results collapse to Inf. But essentially agrees with Table II now!
+
+# Wtilde(x0,a2,Ω)
+xrange=-4:0.1:4
+# These values, from Feynman and Kleinhert, table II
+g=0.1976
+@printf("\n%f %f",g,Wtilde(1.943,0.397,1.255,V,β))
+plot(x->Wtilde(x,0.397,1.255,g,β),xrange,label="g=0.1976") 
+
+g=0.4
+@printf("\n%f %f",g,Wtilde(0.0,1.030,0.486,V,β))
+plot!(x->Wtilde(x,1.030,0.486,g,β),xrange,label="g=0.4") 
+
+g=4.0
+@printf("\n%f %f",g,Wtilde(0.0,0.3059,1.634,V,β))
+plot!(x->Wtilde(x,0.3059,1.634,g,β),xrange,label="g=4.0")
+
+g=40.0
+@printf("\n%f %f",g,Wtilde(0.0,0.1306,3.829,V,β))
+plot!(x->Wtilde(x,0.1306,3.829,g,β),xrange,label="g=40.0")
+
+plot!(ylim=(-2,10)) # force plot in notebook
+
+### OK - let's go for the big one!
+# Try and reproduce Fig 3; Double-well for varying g.
+
+g=0.1976
+β=1000
+
+xrange=-2:0.05:2
+#Ws=[ W(x,g,β) for x in xrange ]
+#println(Ws)
+#plot(Ws,xrange,label="W(x)")
+
+plot(size=(400,500),fmt=:png) # force aspect ratio; 
+    # use PNG as changing size seems to break whatever the default is (SVG?)
+for g in [.2, .3,.35,.4,.6] # straight lines in Fig 3.
+    plot!(x->W(x/sqrt(g),V,β), xrange, label="g=$g") 
+        # Note scaling of x-range within fn. argument
+    plot!(x->V(x/sqrt(g),V), xrange, label="", linestyle = :dash) 
+        # Bare potential = infinite Temperature
+end
+plot!(ylim=(0,1.25)) # same as Fig 3 scale
+
